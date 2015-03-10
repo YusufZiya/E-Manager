7f4c76aa0b2b4dde70add1b843ef7d0700538774
YusufZiya <ziyakorol@hotmail.com>
2015-03-10 14:49:23 +0200

    Add department,employee and IdepartmentDatasource classes

---
+++ eManager.Domain/IDepartmentDataSource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace eManager.Domain
+{
+    public interface IDepartmentDataSource
+    {
+        IQueryable<Employee> Employess { get; set; }
+        IQueryable<Department> Departments { get; set; }
+    }
+}
