d86d74711add9ff61cfc47fcc5f7252796c031b9
YusufZiya <ziyakorol@hotmail.com>
2015-03-10 18:19:03 +0200

    Listing and Detail data

---
+++ eManager.WebUI/Controllers/DepartmnetController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using eManager.Domain;
+using eManager.WebUI.Infrastructure;
+
+namespace eManager.WebUI.Controllers
+{
+    public class DepartmnetController : Controller
+    {
+        private readonly IDepartmentDataSource _db;
+
+        public DepartmnetController(IDepartmentDataSource db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var model = _db._Departments.Single(d => d.Id == id);
+            return View(model);
+        }
+
+    }
+}
